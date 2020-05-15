619933350a3ecad52e43747df9e459d7b4002dc6
Michael Fellinger <michael.fellinger@iohk.io>
2020-05-15 06:41:12 +0200

    also fix yarn2nix

--- nix/default.nix
+++ nix/default.nix
@@ -4,7 +4,7 @@ with {
     inherit (import sources.niv { }) niv;
     packages = pkgs.callPackages ./packages.nix { };
     inherit (import sources.yarn2nix { inherit pkgs; }) yarn2nix mkYarnPackage;
-    yants = pkgs.callPacakge ./yants.nix { };
+    yants = pkgs.callPackage ./yants.nix { };
     lib = pkgs.lib;
     pp = value: builtins.trace (builtins.toJSON value) value;
     compact = pkgs.lib.subtractLists [ null ];
