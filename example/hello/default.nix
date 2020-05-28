d94053887b7993f10a23a84fc7ee36e76ddf3b35
Michael Fellinger <michael.fellinger@iohk.io>
2020-05-29 00:59:08 +0200

    simplify hello world

--- example/hello/default.nix
+++ example/hello/default.nix
@@ -1,21 +1,17 @@
-{ pkgs ? import ../../nix {} }:
 let
   euphenix = import ../.. { };
   inherit (euphenix) build mkPostCSS cssTag;
-
-  globalVariables = {
-    siteName = "Hello";
-    css = cssTag (mkPostCSS ./css);
-  };
-
-  mkRoute = tmpl: vars: {
-    template = [ ./templates/layout.html tmpl ];
-    variables = globalVariables // vars;
-  };
 in build {
   src = ./.;
 
   routes = {
-    "/index.html" = mkRoute ./templates/index.html { title = "Hello World!"; };
+    "/index.html" = {
+      template = [ ./templates/layout.html ./templates/index.html ];
+      variables = {
+        title = "Hello World!";
+        siteName = "Hello";
+        css = cssTag (mkPostCSS ./css);
+      };
+    };
   };
 }
